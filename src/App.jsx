09d8e363000c41af6a433f86ba2df2c97e763236
Prosenjith Dash Pappu <prosenjith.edu@gmail.com>
2025-08-05 12:24:07 +0600

    7-2 Responsive Navbar using react and tailwind  done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Navbar from "./components/Navbar/Navbar"
 
 
 function App() {
 
   return (
     <div>
+      <Navbar/>
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
